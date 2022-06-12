1c3f71a3a64e49294aa2c5c5d55f0a9315285a51
Martin Stabenfeldt <martin@stabenfeldt.net>
2022-06-12 19:37:15 +0200

    Added routes for Products

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -75,9 +75,16 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    #live_session :default, on_mount: PentoWeb.UserAuthLive do
+    live_session :default, on_mount: PentoWeb.UserAuthLive do
       live "/guess", WrongLive
-    #end
+
+      live "/products", ProductLive.Index, :index
+      live "/products/new", ProductLive.Index, :new
+      live "/products/:id/edit", ProductLive.Index, :edit
+
+      live "/products/:id", ProductLive.Show, :show
+      live "/products/:id/show/edit", ProductLive.Show, :edit
+    end
 
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
