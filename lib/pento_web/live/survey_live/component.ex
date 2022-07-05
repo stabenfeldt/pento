74ba7955367f8a3afed56bba1d9b0de5567775f0
Martin Stabenfeldt <martin@stabenfeldt.net>
2022-07-05 15:14:54 +0200

    WIP

--- lib/pento_web/live/survey_live/component.ex
+++ lib/pento_web/live/survey_live/component.ex
@@ -6,6 +6,12 @@ defmodule PentoWeb.SurveyLive.Component do
     <h2>
       content: <%= @content %>
     </h2>
+
+    <br>
+    <br>
+    <h3>
+      slot: <%= render_slot(@inner_block) %>
+    </h3>
     """
   end
 end
