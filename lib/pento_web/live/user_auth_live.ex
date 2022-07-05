3d8fe8599199c412266d3e04d486a2756cd0e66b
Martin Stabenfeldt <martin@stabenfeldt.net>
2022-07-05 12:10:12 +0200

    Use assign_new, which only assigns if the key is not already set.

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -2,24 +2,21 @@ defmodule PentoWeb.UserAuthLive do
   import Phoenix.LiveView
   alias Pento.Accounts
 
-  def on_mount(_, params, %{"user_token" => user_token}, socket) do
+  def on_mount(_, params, %{"user_token" => user_token} = _session, socket) do
+    IO.puts "Assign User with socket.private:"
+    IO.inspect socket.private
+
     user = Accounts.get_user_by_session_token(user_token)
-    socket =
-      socket
-      |> assign(:current_user, user)
+    socket = assign_new(socket, :current_user, 
+      fn -> 
+        Accounts.get_user_by_session_token(user_token)
+      end
+    )
     if socket.assigns.current_user do
       {:cont, socket}
     else
       {:halt, redirect(socket, to: "/login")}
     end
   end
-
-  #def mount(_params, session, socket) do {
-  #  :ok, assign(
-  #    socket,
-  #    score: 0,
-  #    message: "Guess a number.", session_id: session["live_socket_id"]
-  #  ) }
-  #end
 
 end
